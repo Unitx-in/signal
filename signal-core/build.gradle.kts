6a046fc4b68c67e7489a4141a7948ab54f6da5e2
Navneet Sharma <navneet220400@gmail.com>
2026-08-27 11:41:52 +0530

    0.0.5-alpha-02

--- signal-core/build.gradle.kts
+++ signal-core/build.gradle.kts
@@ -51,7 +51,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.unitx.in"
                 artifactId = "signal"
-                version = "0.0.5-alpha-01"
+                version = "0.0.5-alpha-02"
             }
         }
     }
